769d45a0366a23d09b923cac9aa16d7e8f5243ff
Arfat naik <arfatnaik800@gmail.com>
2025-06-22 21:40:05 +0530

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import Hero from './components/hero'
+
+function App() {
+
+  return (
+    <>
+    <main className='relative min-h-screen w-screen overflow-x-hidden' >
+      <Hero/>
+    </main>
+    </>
+  )
+}
+
+export default App
